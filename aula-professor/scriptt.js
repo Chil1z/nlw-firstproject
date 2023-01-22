53b3479411f334943c83374772dfa7303371db44
chilz <ttzchill@gmail.com>
2023-01-22 16:40:56 -0300

    add readme

--- aula-professor/scriptt.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector("#form-habits")
-const nlwSetup = new NLWSetup(form)
-
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
-}
-
-nlwSetup.setData(data)
-nlwSetup.load()
